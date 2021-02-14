112a31dbb0faa00cc2f8ce62862b60b682adca63
Leonardo dos Santos <leodoima@hotmail.com>
2021-02-14 18:06:56 -0300

    Desenvolvimento projeto de interface Twitch

---
+++ src/pages/Following/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Text } from 'react-native';
+import Header from '../../components/Header';
+
+import { Wrapper, Container, Main} from './styles';
+
+const Following: React.FC = () => {
+  return (
+    <Wrapper>
+      <Container>
+        <Header></Header>
+        <Main></Main>
+      </Container>
+    </Wrapper>
+  );
+};
+
+export default Following;
