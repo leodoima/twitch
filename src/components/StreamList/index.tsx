f60269d480a627782061042e47a85c48859f2aa5
Leonardo dos Santos <leodoima@hotmail.com>
2021-02-14 22:01:20 -0300

    Ajustando front-end da aplicacao

---
+++ src/components/StreamList/index.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { 
+  Container, 
+  List, 
+  StreamContainer, 
+  StreamRow, 
+  StreamThumbnail, 
+  StreamColumn, 
+  StreamHeader, 
+  StreamAvatar, 
+  StreamUserName, 
+  StreamDescription, 
+  StreamCategory, 
+  TagRow, 
+  TagView, 
+  TagText } from './styles';
+import streamThumbnail from '../../images/stream_thumbnail.jpg';
+
+const StreamList: React.FC = () => {
+
+  const StreamItem = () => (
+    <StreamContainer>
+      <StreamThumbnail source={streamThumbnail} />
+
+      <StreamColumn>
+        <StreamRow>
+          <StreamHeader>
+            <StreamAvatar />
+            <StreamUserName numberOfLines={1}>leodoima_oficial</StreamUserName>
+          </StreamHeader>
+
+          <StreamDescription numberOfLines={1}>
+            Front-end e Back-end
+          </StreamDescription>
+
+          <StreamCategory numberOfLines={1}>
+            Science & Tech
+          </StreamCategory>
+        </StreamRow>
+
+        <TagRow>
+          <TagView>
+            <TagText>Portuguese</TagText>
+          </TagView>
+          <TagView>
+            <TagText>Web DEv</TagText>
+          </TagView>
+        </TagRow>
+      </StreamColumn>  
+    </StreamContainer>
+  )
+
+  return (
+    <List>
+      <StreamItem />
+      <StreamItem />
+      <StreamItem />
+    </List>
+  );
+};
+
+export default StreamList;
--- src/pages/Following/index.tsx
+++ src/pages/Following/index.tsx
@@ -1,16 +1,91 @@
 import React from 'react';
+import { View , FlatList} from 'react-native';
 
-import { Text } from 'react-native';
 import Header from '../../components/Header';
+import Heading from '../../components/Heading';
+import Title from '../../components/Title';
+import CategoryList from '../../components/CategoryList';
+import StreamList from '../../components/StreamList';
 
 import { Wrapper, Container, Main} from './styles';
 
+interface Item {
+  key: string;
+  render: () => JSX.Element;
+  isTitle?: boolean;
+}
+
 const Following: React.FC = () => {
+  
+  const {data, indices} = React.useMemo(() => {
+    const itens: Item[] = [
+      {
+        key: 'PAGE_HEADING',
+        render: () => <Heading>Following</Heading>,
+      },
+      {
+        key: 'FOLLOWED_CATEGORIES',
+        render: () => <Title>Followed</Title>,
+        isTitle: true
+      },
+      {
+        key: 'C1',
+        render: () => <CategoryList />
+      },
+      {
+        key: 'LIVE_CHANNELS',
+        render: () => <Title>Live Channels</Title>,
+        isTitle: true
+      },
+      {
+        key: 'C2',
+        render: () => <StreamList />
+      },
+      {
+        key: 'CONTINUE_WATCHING',
+        render: () => <Title>Cintinue Watching</Title>,
+        isTitle: true,
+      },
+      {
+        key: 'C3',
+        render: () => <StreamList />
+      },
+      {
+        key: 'OFFLINE_CHANNELS',
+        render: () => <Title>Offline Channels</Title>,
+        isTitle: true,
+      },
+      {
+        key: 'C4',
+        render: () => <View />,
+      },
+    ];
+
+    const indices: number [] = [];
+
+    itens.forEach((item, index) => item.isTitle && indices.push(index));
+
+    return {
+      data: itens,
+      indices,
+    }
+  }, []);
+
   return (
     <Wrapper>
       <Container>
         <Header></Header>
-        <Main></Main>
+        <Main>
+          <FlatList<Item>
+            data={data}
+            renderItem={({item}) => item.render()}
+            keyExtractor={item => item.key}
+            stickyHeaderIndices={indices}
+
+            onRefresh={() => {}}
+            refreshing={false}
+          />
+        </Main>
       </Container>
     </Wrapper>
   );
